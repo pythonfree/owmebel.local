352a9ef22fa2dddd801bf4dd38c14b60c9492f66
shahov <evgeniyshakhov@gmail.com>
2021-10-29 14:46:17 +0300

    Навигация на сайте - меню

---
+++ company/.top.menu.php
@@ -0,0 +1,18 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<?
+$aMenuLinks = array(
+    array(
+        "Компания",
+        "company/",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Новости",
+        "news/",
+        array(),
+        array(),
+        ""
+    ),
+);
