4f71362970382489ec3dec82666759698b605820
Shakhov Evgeniy <79115839983@ya.ru>
2021-11-02 22:33:52 +0300

    Внедерение левого внутреннего меню

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
