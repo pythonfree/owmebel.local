3add71f6dbc21e9a857d2ab7dc33e78282730949
Shakhov Evgeniy <79115839983@ya.ru>
2021-11-03 07:06:38 +0300

    Добавил закрывающий span во внутреннем левом меню

--- local/templates/inner/components/bitrix/menu/inner_left/template.php
+++ local/templates/inner/components/bitrix/menu/inner_left/template.php
@@ -15,15 +15,15 @@ foreach($arResult as $arItem):?>
 		<?if ($arItem["DEPTH_LEVEL"] == 1):?>
 			<li class="<?if ($arItem["SELECTED"]):?>open current<?else:?>close<?endif?>">
                 <span class="sb_showchild"></span>
-                <a href="<?=$arItem["LINK"]?>"><span><?=$arItem["TEXT"]?><span></a>
+                <a href="<?=$arItem["LINK"]?>"><span><?=$arItem["TEXT"]?></span></a>
 				<ul>
 		<?endif?>
 	<?else:?>
         <?if ($arItem["PERMISSION"] > "D"):?>
             <?if ($arItem["DEPTH_LEVEL"] == 1):?>
-                <li class="close  <?if ($arItem["SELECTED"]):?>current<?endif?>"><a href="<?=$arItem["LINK"]?>"><span><?=$arItem["TEXT"]?></span></a></li>
+                <li class="close<?if ($arItem["SELECTED"]):?> current<?endif?>"><a href="<?=$arItem["LINK"]?>"><span><?=$arItem["TEXT"]?></span></a></li>
             <?else:?>
-                <li class="<?if ($arItem["SELECTED"]):?>close current<?endif?>"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <li<?if ($arItem["SELECTED"]):?> class="close current"<?endif?>><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
             <?endif?>
         <?endif?>
 	<?endif?>
