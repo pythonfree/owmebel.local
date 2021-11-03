57118cc658f5141d134be7224dfe6ee2a5cd8dfb
Shakhov Evgeniy <79115839983@ya.ru>
2021-11-03 09:45:47 +0300

    10. Поиск

--- local/templates/.default/include/header.php
+++ local/templates/.default/include/header.php
@@ -28,12 +28,14 @@
                     <?=GetMessage('WORKING_TIME');?> <span class="workhours">ежедневно с 9-00 до 18-00</span>
                 </td>
                 <td style="width:232px">
-                    <form action="">
-                        <div class="hd_search_form" style="float:right;">
-                            <input placeholder="Поиск" type="text"/>
-                            <input type="submit" value=""/>
-                        </div>
-                    </form>
+
+                    <?$APPLICATION->IncludeComponent("bitrix:search.form", "head", Array(
+                        "PAGE" => "#SITE_DIR#search/index.php",	// Страница выдачи результатов поиска (доступен макрос #SITE_DIR#)
+                        "USE_SUGGEST" => "N",	// Показывать подсказку с поисковыми фразами
+                    ),
+                        false
+                    );?>
+
                 </td>
             </tr>
             <tr>
--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?>
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent("bitrix:search.form", "head", Array(
+	"PAGE" => "#SITE_DIR#search/index.php",	// Страница выдачи результатов поиска (доступен макрос #SITE_DIR#)
+		"USE_SUGGEST" => "N",	// Показывать подсказку с поисковыми фразами
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
